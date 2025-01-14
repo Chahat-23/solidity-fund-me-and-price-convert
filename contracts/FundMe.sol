12d9f909eda07b575a479ebc64462af02cb3eab2
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-01-14 15:25:00 +0530

    'eth to usd price convert with chainlink API'

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
@@ -8,6 +9,28 @@ contract FundMe {
     function fundMe() public payable{
         //set minimum fund amount
         require(msg.value > minUsd, "Didn't send enough");
+        //18 decimals
     } 
+
+    function getPrice() public view returns (uint) {
+        //address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI - 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,) = priceFeed.latestRoundData();
+        //8 decimals
+        return uint(price * 1e10);
+    }
+
+    function getVersion() public view returns (uint) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint ethAmount) public view returns (uint) {
+        uint ethPrice = getPrice();
+        uint ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        //36 decimals because of *, so we half it to match msg.value decimals
+        return ethAmountInUsd;
+    }
     //function withdraw() {}
 }
